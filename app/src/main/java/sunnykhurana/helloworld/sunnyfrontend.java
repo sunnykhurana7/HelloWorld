572596823a88beade951d30f88380e4a2320e0e4
sunnykhurana7 <sunnycreative7@gmail.com>
2016-11-05 14:24:48 +0530

    added a comment to the main file and add a new java class

---
+++ app/src/main/java/sunnykhurana/helloworld/sunnyfrontend.java
@@ -0,0 +1,12 @@
+package sunnykhurana.helloworld;
+
+/**
+ * Created by SUNNY on 05-11-2016.
+ */
+
+public class sunnyfrontend {
+    @Override
+    public String toString() {
+        return "sunny khurana";
+    }
+}
